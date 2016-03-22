c4b8b3895fe7261bf9c8be3d5fe5637bf2e18b45
georgesotirov <george.g.sotirov@gmail.com>
2016-03-22 20:14:22 +0200

    Fixed fb integration

--- Source/Trunk/TrainWithMe/WebContent/pages/header.jsp
+++ Source/Trunk/TrainWithMe/WebContent/pages/header.jsp
@@ -48,17 +48,7 @@
 
 <script>
 
-$(document).ready(function() {
-	  $.ajaxSetup({ cache: true });
-	  $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
-	    FB.init({
-	      appId: '1700510940234048',
-	      version: 'v2.5' // or v2.0, v2.1, v2.2, v2.3
-	    });     
-	    $('#loginbutton,#feedbutton').removeAttr('disabled');
-	    FB.getLoginStatus(updateStatusCallback);
-	  });
-	});
+
 </script>
 </head>
 
--- Source/Trunk/TrainWithMe/WebContent/resources/mainTheme/js/facebook_api.js
+++ Source/Trunk/TrainWithMe/WebContent/resources/mainTheme/js/facebook_api.js
@@ -11,12 +11,12 @@ function statusChangeCallback(response) {
 		testAPI();
 	} else if (response.status === 'not_authorized') {
 		// The person is logged into Facebook, but not your app.
-		document.getElementById('status').innerHTML = 'Please log '
+		document.getElementById('statusa').innerHTML = 'Please log '
 				+ 'into this app.';
 	} else {
 		// The person is not logged into Facebook, so we're not sure if
 		// they are logged into this app or not.
-		document.getElementById('status').innerHTML = 'Please log '
+		document.getElementById('statusa').innerHTML = 'Please log '
 				+ 'into Facebook.';
 	}
 }
@@ -33,8 +33,10 @@ function checkLoginState() {
 window.fbAsyncInit = function() {
 	FB.init({
 		appId : '1700510940234048',
-		xfbml : true,
-		version : 'v2.5'
+		cookie : true, // enable cookies to allow the server to access
+		// the session
+		xfbml : true, // parse social plugins on this page
+		version : 'v2.5' // use graph api version 2.5
 	});
 
 	// Now that we've initialized the JavaScript SDK, we call
@@ -58,9 +60,8 @@ window.fbAsyncInit = function() {
 // Load the SDK asynchronously
 (function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) {
+	if (d.getElementById(id))
 		return;
-	}
 	js = d.createElement(s);
 	js.id = id;
 	js.src = "//connect.facebook.net/en_US/sdk.js";
@@ -71,9 +72,19 @@ window.fbAsyncInit = function() {
 // successful. See statusChangeCallback() for when this call is made.
 function testAPI() {
 	console.log('Welcome!  Fetching your information.... ');
-	FB.api('/me', function(response) {
-		console.log('Successful login for: ' + response.name);
-		document.getElementById('status').innerHTML = 'Thanks for logging in, '
-				+ response.name + '!';
+	FB
+			.api(
+					'/me',
+					function(response) {
+						console.log('Successful login for: ' + response.name);
+						document.getElementById('status').innerHTML = '<img src="//graph.facebook.com/'
+								+ response.id + '/picture"><br><a href="javascript:void(0)" onclick="fbLogout()">Logout</a>';
+					});
+}
+
+function fbLogout() {
+	FB.logout(function(response) {
+		// user is now logged out
+		location.reload();
 	});
 }
